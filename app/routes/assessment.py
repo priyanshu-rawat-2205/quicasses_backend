944efb91071d64066db4a842af412003b78a8905
Priyanshu Rawat <dev.priyanshurawat@gmail.com>
2025-02-17 21:32:14 +0530

    added CRUD of Assessment

--- app/routes/assessment.py
+++ app/routes/assessment.py
@@ -21,4 +21,74 @@ def create():
     db.session.commit()
 
     return jsonify(new_assessment.to_dict()), 201    
-    
+
+
+#Read by ID   
+@assessment_bp.route("/<uuid:uuid>", methods=['GET'])
+@jwt_required()
+def read(uuid):
+    from app.models import Assessment
+
+    assessment = Assessment.query.get(uuid)
+    if not assessment:
+        return jsonify({"msg": "Assessment not found"}), 404
+    return jsonify(assessment.to_dict()), 200
+
+
+@assessment_bp.route("/<uuid:uuid>", methods=["PUT"])
+@jwt_required()
+def update(uuid):
+    from app.models import db, Assessment
+
+    assessment = Assessment.query.get(uuid)
+    if not assessment:
+        return jsonify({"msg": "Assessment not found"}), 404
+
+    user_id = get_jwt_identity()
+    if assessment.creator_id != user_id:
+        return jsonify({"msg": "Unauthorized"}), 403
+
+    data = request.get_json()
+    assessment.title = data.get("title", assessment.title)
+    assessment.description = data.get("description", assessment.description)
+    assessment.questions = data.get("questions", assessment.questions)
+
+    db.session.commit()
+    return jsonify(assessment.to_dict()), 200
+
+
+@assessment_bp.route("/<uuid:uuid>", methods=["DELETE"])
+@jwt_required()
+def delete(uuid):
+    from app.models import db, Assessment
+
+    assessment = Assessment.query.get(uuid)
+    if not assessment:
+        return jsonify({"msg": "Assessment not found"}), 404
+
+    user_id = get_jwt_identity()
+    if assessment.creator_id != user_id:
+        return jsonify({"msg": "Unauthorized"}), 403
+
+    db.session.delete(assessment)
+    db.session.commit()
+    return jsonify({"msg": "Assessment deleted successfully"}), 200
+
+@assessment_bp.route('/my-assessments', methods=['GET'])
+@jwt_required()  
+def read_my_assessments():
+    from app.models import Assessment
+
+    user_id = get_jwt_identity()
+    assessments = Assessment.query.filter_by(creator_id=user_id).all()
+
+    if not assessments:
+        return jsonify({'msg':'You have not created any assessments.'})
+
+    return jsonify([{
+        "uuid": assessment.uuid,
+        "title": assessment.title,
+        "description": assessment.description,
+        "questions": assessment.questions,
+        "created_at": assessment.created_at
+    } for assessment in assessments]), 200
